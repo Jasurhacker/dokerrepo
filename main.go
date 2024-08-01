560f2a6a20c27bcec50d9db0a4828fb76c9776a3
Your Name <you@example.com>
2024-08-01 19:34:09 +0500

    первый коммит

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import(
+	"github.com/gin-gonic/gin"
+)
+
+func list(c *gin.Context)  {
+	c.JSON(200 , "list")
+	print("list")
+}
+
+func add(c *gin.Context)  {
+	c.JSON(200 , "list")
+	print("list")
+}
+
 func main() {
-	
+	route := gin.Default()
+
+	route.GET("/list",list)
+	route.POST("/add",add)
+
+	route.Run(":2010")
 }
